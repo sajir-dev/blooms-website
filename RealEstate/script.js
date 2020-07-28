4c97ce1e6860ec852ae5cfb22f99aed8634a903d
hima-del <himajaharidas77@gmail.com>
2020-07-28 16:10:47 +0530

    radio button added

--- RealEstate/script.js
+++ RealEstate/script.js
@@ -15,15 +15,15 @@ function myFunction3() {
     document.getElementById("myDropdown2").classList.remove("show");
     document.getElementById("myDropdown1").classList.remove("show");
 }
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        let dropdowns = document.getElementsByClassName("dropdown-content");
-        let i;
-        for (i = 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         let dropdowns = document.getElementsByClassName("dropdown-content");
+//         let i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             let openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
